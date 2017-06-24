aa4d98b4009871fa087b2afe867d6c87812c1557
Blade Wolfmoon <2CPT1MP@gmail.com>
2017-06-24 19:25:59 +0300

    reqguests

---
+++ req/script.js
@@ -0,0 +1,18 @@
+var inp = document.getElementById("5582");
+
+inp.onclick = function () {
+   alert("Pornhub is currently unavailable");
+}
+
+var inp1 = document.getElementById("1082");
+inp1.onclick = function () {
+	var passcode = prompt("Enter your request code");
+	
+	if (passcode == "5F85100") {
+	    alert("Library Card Number: \nPIN: ")
+	}
+	
+	
+	
+}
+
